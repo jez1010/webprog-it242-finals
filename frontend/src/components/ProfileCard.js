fd935dc67297a7a8c5eb196026f53ed03452cd8c
Jez <146207614+jez1010@users.noreply.github.com>
2026-02-23 16:04:45 +0000

    More CSS added, decided on Win98 style UI.

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,74 +1,43 @@
 import styles from './ProfileCard.module.css';
 import profile from '../assets/content/profile.png';
+import close from '../assets/content/exit.webp';
+import max from '../assets/content/maximize.webp';
+import min from '../assets/content/minimize.webp';
 
 function ProfileCard() {
   return (
-    <div
-      class={styles.body_div}
-      id="top_section"
-    >
-      <div class={styles.title}>
-        About Me
+    <div className = {styles.profile_card}>
+      <div className = {styles.top_tab}>
+        <div>My Profile</div>
+        <div className = {styles.buttons}>
+          <img src = {min} alt = "min"/>
+          <img src = {max} alt = "max"/>
+          <img src = {close} alt = "close"/>                    
+        </div>
       </div>
 
-      <div class={styles.profile_div}>
-        <div class={styles.side_items} id="paragraph">
-
-          Jezron D. Cardona is a 2nd year student of Bachelor of Science in Information Technology (BSIT) studying currently at Asia Pacific College.
-          He has done several projects, both as hobbies and school requirements. His current goals in life is to be a known cybersecurity expert or a
-          full-stack developer. His current aspirations, as he is currently in the middle of deciding, is to to own a two-floor property and gain stable income.
-
+      <div className = {styles.header}>
+        <div className = {styles.image_cover}>
+          <img src={profile} alt="Profile page"/>
         </div>
-
-        <div class={styles.central_profile}>
-          <img src={profile} alt="Webpage creator profile."/>
+        <div className = {styles.names}>
+          <div className = {styles.fullname}>Jezron D. Cardona</div>
+          <div className = {styles.email}>jdcardona@student.apc.edu.ph</div>
         </div>
-
-        <div class={styles.side_items} id="details">
-          <div class={styles.title}>Name:</div>
-          <p>Jezron D. Cardona</p>
-
-          <div class={styles.title}>Age:</div>
-          <p>18 years <span class="subtext">(As of January 1, 2026)</span></p>
-
-          <div class={styles.title}>Education:</div>
-          <ul>
-            <li>SHS: Pasay City Science National High School.
-              <ul>
-                <li>Track: Science, Technology, Engineering, Programing and Mathematics.</li>
-              </ul>
-            </li>
-            <li>College: Asia Pacific College <span class="subtext">(Current)</span>
-              <ul>
-                <li>Course: Bachelor of Science in Information Technology.</li>
-              </ul>
-            </li>
-          </ul>
-
-          <div class={styles.contacts_row}>
-            <div>
-              <a href="https://www.facebook.com/jezcardz">
-                <img
-                  src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/facebook-app-round-white-icon.png"
-                  class={styles.contacts_logo}
-                  alt="Facebook logo. Click to see their Facebook profile."
-                />
-              </a>
-            </div>
-            <div>
-              <a href="https://www.linkedin.com/in/jezron-cardona-590939321/">
-                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-white-icon.png" class={styles.contacts_logo} alt="LinkedIn logo. Click to see their LinkedIn profile." />
-              </a>
-            </div>
-            <div>
-              <img src="https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/phone-call-white-icon.png" class={styles.contacts_logo} alt="A graphic of a telephone." />
-              +63 992 722 9820
-            </div>
-          </div>
-
-        </div>
-
       </div>
+
+      <div className = {styles.desc}>
+        <div className = "label">About Me</div>
+
+        <div className = "text">
+          Jezron D. Cardona is a 2nd year student currently studying for a degree in Bachelor of Science in 
+          Information Technology (BSIT) at Asia Pacific College. He has done several projects, both as hobbies 
+          and school requirements. His dream is to get into a presitigious company such as IBM, a company that 
+          he had held fascination for since the start of his IT career. His other aspirations include acquiring 
+          a humble property for independent living.
+        </div>
+      </div>
+
     </div>
   );
 }
